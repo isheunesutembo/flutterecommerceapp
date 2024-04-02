ecd3a7f36312d188c634ab8029388dbdbc80ee43
Isheunesu Tembo <isheunesu48@gmail.com>
2024-04-02 22:10:52 +0300

    Update apikey.dart

--- lib/apikeys/apikey.dart
+++ lib/apikeys/apikey.dart
@@ -1,5 +1,5 @@
 class APIKey {
   static const STRIPE_SECRET =
-      "sk_test_51DFLIGJVwTUSWUt4hnBDwUpYHYCv7wWNMIX1FHoyUIwstGcvAISAlfYxRGAU4tz4ddXsNP5Q1599bIBvRffIF5pI00lTZZLZdZ";
-  static const PUBLISHABLEkEY = "pk_test_YB5q2AxlwiIQihPRQEFlzPp5";
+      "Your key";
+  static const PUBLISHABLEkEY = "Your Key";
 }
